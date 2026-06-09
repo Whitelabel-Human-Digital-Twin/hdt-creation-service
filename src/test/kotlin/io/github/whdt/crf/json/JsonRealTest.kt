c9094dd123a0e57356bfd527399131d0dd882556
lm98 <leonardomicelli@gmail.com>
2026-06-09 13:30:37 +0200

    test: add actual json test

---
+++ src/test/kotlin/io/github/whdt/crf/json/JsonRealTest.kt
@@ -0,0 +1,36 @@
+package io.github.whdt.crf.json
+
+import io.github.ktwinx.core.hdt.HdtId
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.jsonObject
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class JsonRealTest {
+    @Test
+    fun `maps HDT correctly from real JSON`() {
+        val resourceName = "/NW_ChoRod.json"
+        val inputString = this::class.java.getResource(resourceName)
+            ?.readText()
+            ?: error("Resource not found: $resourceName")
+        val jsonObject = Json.parseToJsonElement(inputString).jsonObject
+
+        val assembler = JsonDomainAssembler()
+        val result = assembler.assemble(jsonObject)
+
+        // assert only three models imported
+        val modelNames = listOf("root", "temporal", "nonLinear")
+        val actualModelNames = result.models.map { it.name.value }
+        assertEquals(modelNames, actualModelNames)
+
+        // assert models are not empty
+        result.models.forEach {
+            assertTrue{ it.properties.isNotEmpty() }
+        }
+
+        // assert correct HdtId
+        val hdtId = HdtId("NW_ChoRod")
+        assertEquals(hdtId.id, result.hdtId)
+    }
+}
